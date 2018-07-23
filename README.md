05e9c29dd96bb1578094a94cade9e5ee4aa55813
Skeletor <johanneskaunisvaara@gmail.com>
2018-07-23 15:57:12 +0300

    geometry fixes

--- README.md
+++ README.md
@@ -9,13 +9,13 @@ for generating svg starmaps from selected coordinates and time
 	python -coord [northern,eastern] -time [hour.minute.second] -date [day.month.year] -utc [utc]
 
 	optional parameters
-		-o 
-		-width = 200. width in mm
-		-height = 200. height in mm
-		-magnitudelimit = 4.5 from 0-7.0
-		-backgroundcolor = rgb(45,59,98) rgb(r,g,b)
-		-starcolor = rgb(255,255,255)
-		-infotext = HELSINKI NIGHT SKY
+		-o = starmap.svg. Output file name
+		-width = 200. Width in mm
+		-height = 200. Height in mm
+		-magnitudelimit = 4.5.Magnitude limit is from 0-7.0. 0 is brightest, 7.0 dimmest
+		-backgroundcolor = rgb(45,59,98).  rgb(r,g,b)
+		-starcolor = rgb(255,255,255), rgb(r,g,b)
+		-infotext = HELSINKI NIGHT SKY, Text that is displayed in the corner of the image
 
 ## Example
 	 python starmap.py -coord 45.28,11.08 -time 12.30.30 -date 12.01.2011 -utc +2
--- starmap.py
+++ starmap.py
@@ -4,11 +4,11 @@ import random
 import math
 import argparse
 
+############ DEFAULT VALUES AND CONSTS ####################################
+
 PI = 3.141592
 
-#Stars declination and hour data file "Yale Bright Star Catalog 5"
-file = "datafiles/ybsc5.txt"
-file2 = "datafiles/stardata.txt"
+font_style = "font-size:10px; letter-spacing:0.7px; font-family:sans-serif; stroke-width:5;font-weight:bold;"
 
 background_color = "rgb(45,59,98)"
 line_color = "rgb(255,255,255)"
@@ -17,27 +17,49 @@ output_file = 'starmap.svg'
 
 #Date & Time
 date = '01.01.2000' 
-time = '00.00'
-utc = 2
+time = '12.00'
+utc = 0
 summertime = False
 
 #Coordinates
 coord = "60.186,24.959"
+coord = "52.00,1.0"
+
 
 #placetext for leftdown corner
-place = 'HELSINKI NIGHT SKY'
+info = 'HELSINKI NIGHT SKY'
 
 #Size of poster in mm
 width = 200
 height = 200
 
+def mm_to_px(mm):
+	px = mm*96/25.4
+	return px
+
 #Smaller the star bigger the magnitude
-magnitude_limit = 5.5
-aperture = 0.2 
+magnitude_limit = 3.5
+aperture = 0.4 
 amplification = width*0.045
 
+############ STARDATAFILE ################################################
 
-############ ARGPARSER #####################################################
+#Stars declination and hour data file "Yale Bright Star Catalog 5"
+file = "datafiles/ybsc5.txt"
+file = "datafiles/stardata.txt"
+
+#read data from file to nested list
+def read_star_coordinate_file(file):
+	data = []
+	with open(file, 'rt') as f:
+		for line in f:
+			if (',' in line): 
+				data.append([ n for n in line.strip().split(',')])
+	return data
+
+data = read_star_coordinate_file(file)
+
+############ ARGPARSER ###################################################
 
 parser = argparse.ArgumentParser(description='Generate starmap svg file')
 parser.add_argument('-coord','--coord', help='coordinates in format northern,eastern',default=coord )
@@ -49,7 +71,6 @@ parser.add_argument('-o','--output', help='output filename.svg',default='starmap
 parser.add_argument('-width','--width',nargs='?', help='width in mm',type=int, default=width)
 parser.add_argument('-height','--height',nargs='?', help='height in mm',type=int, default=height)
 parser.add_argument('-magn','--magn',nargs='?', help='magnitude limit',type=int, default=magnitude_limit)
-
 
 args = parser.parse_args()
 
@@ -67,92 +88,9 @@ print(coord)
 print(date)
 print(time)
 
-
 northern,eastern = map(float,coord.split(','))
 
-#error bias in time
-correction = -16.7
-utc += correction
-
-############################################################################
-
-def mm_to_px(mm):
-	px = mm*96/25.4
-	return px
-
-#date to days
-def date_and_time_to_rad(date,time):
-
-	days_in_year = 365 # days in normal year
-	months = [31,28,31,30,31,30,31,31,30,31,30,31] #Array of days in months
-	year = int(date[6:10])
-	
-	#Leap year
-	if(year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
-		months[1] = 29 
-		days_in_year = 366
-
-	#TODO yearly mistake epoch
-	degree = -(year-2000)*360/26000
-	
-
-	days  = sum(months[0:int(date[3:5])-1])	#month to days
-	days += int(date[0:2])-1					#days
-
-	#calculate degree from days
-	degree += (days)*360.0/days_in_year
-
-	minutes  = float(time[0:2])*60  		#hour
-	minutes += float(time[3:5])	  			#minute
-
-	#Summertime
-	if(summertime):
-		minutes -= 60
-
-	#UTC
-	minutes -= 60*utc
-
-	#calculate degree from minutes
-	degree += (minutes*360/(24*60-4)) % 360
-
-	return math.radians(degree)
-
-
-
-def weekday(date,lang):
-
-	month_day_add = [0,3,3,6,1,4,6,2,5,0,3,5]
-	days_en = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-	days_fi = ['Maanantai','Tiistai','Keskiviikko','Torstai','Perjantai','Lauantai','Sunnuntai']
-
-	day = int(date[0:2])
-	month = int(date[3:5])
-	year = int(date[6:10])
-	
-	#		vuosi		karkausvuosi	paiva 	kuukausi					lauantai
-	add = (	year-2000 +(year-2000)//4 + day-1 + month_day_add[month-1] + 	6) % 7
-
-	#karkausvuosi specialcase
-	if year % 4 == 0 and month < 3:
-		add -= 1
-
-	# print(days_fi[add])
-
-#change polar coordinates to cartesian coordinates
-def polar_to_cartesian(radius,angle,centerx,centery):
-	return [centerx + radius*math.cos(angle), centery + radius*math.sin(angle)]
-
-
-
-#read data from file to nested list
-def read_star_coordinate_file(file):
-	data = []
-	with open(file, 'rt') as f:
-		for line in f:
-			if (',' in line): 
-				data.append([ n for n in line.strip().split(',')])
-	return data
-
+########## DRAWING FUNCTIONS  ###########################################
 
 # Generates random star shape to given coordinate and magnitude and color
 def star_generator(x,y,mag,color):
@@ -178,6 +116,56 @@ def dot_generator(x,y,mag,color):
 	image.add(image.circle((x,y),mag,id ='dot',stroke="none",fill=color))
 
 
+########## TIME CALCULATION  ###########################################
+
+#date to days
+def date_and_time_to_rad(date,time):
+
+	days_in_year = 365 # days in normal year
+	months = [31,28,31,30,31,30,31,31,30,31,30,31] #Array of days in months
+
+	year = int(date[6:10])
+	#Leap year
+	if(year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
+		months[1] = 29 
+		days_in_year = 366
+
+	days  = sum(months[0:int(date[3:5])-1])	#month to days
+	days += int(date[0:2])-1				#days
+
+	minutes  = float(time[0:2])*60  		#hour
+	minutes += float(time[3:5])	  			#minute
+	
+	#Summertime
+	if(summertime):
+		minutes -= 60
+	#UTC
+	minutes += 60*utc
+
+	#TODO yearly mistake epoch
+	#Epoch J2000 12.00
+
+	#calculatr degree from years -EPOCH 2000 year
+	degree = -(year-2000)*360/26000
+	
+	print(degree)
+	#calculate degree from days
+	degree -= (days)*360.0/days_in_year
+	print(degree)
+	#calculate degree from minutes -EPOCH 12 hour
+	degree -= ((minutes-12*60)*360/(24*60-4)) % 360
+	print(degree)
+
+	return math.radians(degree)
+
+
+########## POSITION CALCULATION  ###########################################
+
+#change polar coordinates to cartesian coordinates
+def polar_to_cartesian(radius,angle,centerx,centery):
+	return [centerx + radius*math.cos(angle), centery + radius*math.sin(angle)]
+
+
 def distance_between(north,east,dec_angle,ra_angle):
 	delta_ra = ra_angle - east
 	rad = math.acos(math.cos(delta_ra)*math.cos(north)*math.cos(dec_angle) + math.sin(north)*math.sin(dec_angle))
@@ -199,16 +187,21 @@ def declination_to_rad(dec):
     return math.radians(float(dec))
 
 def rad_to_xy(north,east, dec_angle, ra_angle):
-    delta_ra = ra_angle - east
+    delta_ra = ra_angle-east
     x = math.cos(dec_angle) * math.sin(delta_ra)
     y = math.sin(dec_angle) * math.cos(north) - math.cos(dec_angle) * math.cos(delta_ra) * math.sin(north)
     return x * 300 ,y * 300
 
 
+
+
+########## STAR AND GUIDE GENERATION  ########################################
+
 def generate_guides(northern_N,eastern_E):
 
 	N = math.radians(northern_N)
 	E = math.radians(eastern_E)
+
 
 	draw_guides = []
 	for angle_test in range(0,240):
@@ -224,27 +217,27 @@ def generate_guides(northern_N,eastern_E):
 		ascension = right_ascension_to_rad(line[1])
 		
 		#magnitude of dot
-		magn = 0.7
+		magn = 1.1
 		radius = distance_between(N,E,declination,ascension)
 		x,y = rad_to_xy(N,E,declination,ascension)
 
 		if (radius < (90*PI/180.0)):
-			dot_generator(half_x+x,half_y+y,magn*aperture,line_color)
+			dot_generator(half_x-x,half_y-y,magn*aperture,line_color)
 
 def generate_starmap(northern_N,eastern_E,date,time):
 
-	data = read_star_coordinate_file(file2)
+	
 	N = math.radians(northern_N)
 	E = math.radians(eastern_E)
 
+	raddatetime = date_and_time_to_rad(date,time)
 
 	for line in data:
 		if(float(line[2]) < magnitude_limit):
-		# if(float(line[5]) < magnitude_limit):
 
 			#star position from datafile
 			# ascension = right_ascension_to_rad(hours_to_decimal(line[0]))
-			ascension = right_ascension_to_rad(float(line[0]))+ date_and_time_to_rad(date,time)
+			ascension = right_ascension_to_rad(float(line[0]))+raddatetime
 			declination = declination_to_rad(line[1])
 
 			x,y = rad_to_xy(N,E,declination,ascension)
@@ -256,16 +249,24 @@ def generate_starmap(northern_N,eastern_E,date,time):
 			magn = 6 - 2.5*math.log(2+magn,10)
 
 
-			if (radius < 89*PI/180.0):
+			if (radius < 90*PI/180.0):
 				if (magn < 3):
-					dot_generator(half_x+x,half_y+y,0.4*aperture,line_color)
+					dot_generator(half_x-x,half_y-y,0.4*aperture,line_color)
 				else:
-					star_generator(half_x+x,half_y+y,magn*aperture,line_color)
+					star_generator(half_x-x,half_y-y,magn*aperture,line_color)
 
 				#North Star 
 				if(declination > 89.1*PI/180.0):
-				 	star_generator(half_x+x,half_y+y,magn*aperture,"red")
+				 	star_generator(half_x-x,half_y-y,magn*aperture,"red")
 
+				if len(line) > 3:
+					image.add(image.text('x', insert=(half_x-x,half_y-y), fill=line_color, style=font_style))
+
+
+
+
+
+########## MAIN FUNCTION  ###########################################
 
 
 if __name__ == '__main__':
@@ -287,8 +288,8 @@ if __name__ == '__main__':
 	generate_starmap(northern,eastern,date,time)
 
 	#Text in bottom corner
-	font_style = "font-size:10px; letter-spacing:0.7px; font-family:sans-serif; stroke-width:5;font-weight:bold;"
-	image.add(image.text(place, insert=("20mm", str(height-21)+'mm'), fill=line_color, style=font_style))
+
+	image.add(image.text(info, insert=("20mm", str(height-21)+'mm'), fill=line_color, style=font_style))
 	image.add(image.text(str(northern)+" N "+str(eastern)+" E " , insert=("20mm", str(height-17)+'mm'), fill=line_color, style=font_style))
 	image.add(image.text(date +" "+ time, insert=("20mm", str(height-13)+'mm'), fill=line_color, style=font_style))
 
